51101a0bc47a1732cb950647d6a1ac0ca7270fc3
yogekgagdev <yogekgarg17@gmail.com>
2023-09-10 14:53:44 +0530

    adding contracts records and authenticationcontroller

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest registerRequest)
+        {
+            return Ok(registerRequest); 
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            return Ok(loginRequest);
+        }
+    }
+}
